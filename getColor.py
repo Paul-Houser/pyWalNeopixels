403a71e2c2ef18a6a8cf6726ee2b80737e9b3f7c
Paul Houser <houserp@wwu.edu>
2019-09-30 21:57:23 -0700

    deleted files outside raspi directory

--- getColor.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def get_hex_code(colorName, directory):
-    with open(directory) as json_file:
-        colors = json.load(json_file)["colors"]
-        hexString = colors[colorName]
-        return hexString
